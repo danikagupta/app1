6b1bee34ed110d314fdc63a69f86893ea70e804e
Danika Gupta <danika@gprof.com>
2022-10-06 19:54:07 -0700

    Initial Commit

---
+++ app1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app1
+//
+//  Created by Dani Gupta on 10/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by Dani Gupta on 10/6/22.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
