3b649386dec96eaa5da9f01772a27e1e071087e3
Danika Gupta <danika@gprof.com>
2022-10-06 20:04:26 -0700

    changed to multi tab

---
+++ app1/webviewcore.swift
@@ -0,0 +1,29 @@
+//
+//  webviewcore.swift
+//  app1
+//
+//  Created by Dani Gupta on 10/6/22.
+//
+import SwiftUI
+import WebKit
+ 
+struct WebView: UIViewRepresentable {
+ 
+    var url: URL
+ 
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+ 
+    func updateUIView(_ webView: WKWebView, context: Context) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+        print("Loaded request for URL=",url)
+    }
+}
+
+struct WebView_Previews: PreviewProvider {
+    static var previews: some View {
+        WebView(url:URL(string:"https://www.cnn.com")!)
+    }
+}
